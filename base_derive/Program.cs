13b100172c038ac856faff800a4bf9b152267d1b
Ikram_Lim <ikramlim@gmail.com>
2015-12-19 00:01:37 +0800

    Base_Derived
    Base_Derived

---
+++ base_derive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base_derive
+{
+    class Baseclass
+    {
+        int i;
+        public Baseclass(int ii)
+        {
+            i = ii;
+            Console.Write("Base");
+        }
+    }
+    class Derived: Baseclass
+    {
+        public Derived(int ii):base(ii)
+        {
+            Console.Write("Derived \n");
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived(20);
+        }
+        
+    }
+}
